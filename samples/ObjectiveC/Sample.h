2f097006ea092a78c0662817c9f65e7320656889
cgrente <Zack@Cliffs-MacBook-Pro.local>
2014-03-28 18:50:32 +0100

    add items

---
+++ samples/ObjectiveC/Sample.h
@@ -0,0 +1,25 @@
+/******************************************************************************\
+* Copyright (C) 2012-2013 Leap Motion, Inc. All rights reserved.               *
+* Leap Motion proprietary and confidential. Not for distribution.              *
+* Use subject to the terms of the Leap Motion SDK Agreement available at       *
+* https://developer.leapmotion.com/sdk_agreement, or another agreement         *
+* between Leap Motion and you, your company or other organization.             *
+\******************************************************************************/
+
+#import <Foundation/Foundation.h>
+#import <AVFoundation/AVFoundation.h>
+#import "LeapObjectiveC.h"
+
+@interface Sample : NSObject<LeapListener> {
+    
+    BOOL hadChanged;
+    AVAudioPlayer* audioplayer;
+    NSString *path;
+    NSURL* file;
+    NSArray * tableSimple;
+    int i;
+}
+
+-(void)run;
+
+@end
